e66d01803d903885574371e43b75f3d0e84752ce
negigau-hub <g90negi@gmail.com>
2026-03-14 12:52:46 +0530

    log path changed.

--- app/lib/config/config.py
+++ app/lib/config/config.py
@@ -6,7 +6,7 @@ class Config:
 		DB_HOST = os.getenv('DB_HOST', 'lab-db.postgres.database.azure.com')
 		DB_PORT = int(os.getenv('DB_PORT', 5432))
 		DB_NAME = os.getenv('DB_NAME', 'flaskappdb')
-		LOG_DIR = os.getenv('LOG_DIR', '/home/azureuser/myapi/logs')
+		LOG_DIR = os.getenv('LOG_DIR', '/home/LogFiles')
 	else:
 		DB_HOST = os.getenv('DB_HOST')
 		DB_PORT = int(os.getenv('DB_PORT', 5432))
