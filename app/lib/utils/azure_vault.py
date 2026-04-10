898cabae63d7f30d236b45491dd53a29f44c068a
negigau-hub <g90negi@gmail.com>
2026-04-10 21:14:53 +0530

    allowed azure vault to web app.

--- app/lib/utils/azure_vault.py
+++ app/lib/utils/azure_vault.py
@@ -1,4 +1,5 @@
-from azure.identity import ManagedIdentityCredential
+from azure.identity import DefaultAzureCredential
+#from azure.identity import ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
 
 def get_db_credentials():
@@ -8,7 +9,8 @@ def get_db_credentials():
 		tuple: (db_username, db_password)
 	"""
 	vault_url = "https://lab-key-vault001.vault.azure.net/"
-	credential = ManagedIdentityCredential(client_id="f94f8e62-21ac-4eef-8faf-1ded9fb8e33d")
+	#credential = ManagedIdentityCredential(client_id="f94f8e62-21ac-4eef-8faf-1ded9fb8e33d")
+	credential = DefaultAzureCredential()
 	client = SecretClient(vault_url=vault_url, credential=credential)
 	db_username = client.get_secret("db-username").value
 	db_password = client.get_secret("db-password").value
